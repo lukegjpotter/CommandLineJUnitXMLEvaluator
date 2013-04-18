2a89389daa52be74bc0a8ab20bb449199b7bad15
Luke GJ Potter <lukegjpotter@gmail.com>
2013-04-18 23:36:11 +0100

    Updated Test runner to test exit conditions for bad arguments.

--- test_runner.sh
+++ test_runner.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-# TODO Make this executable from anywhere
+# TODO Make this script execute the tests from anywhere
+
+clear
+
+echo "Evaluating no args"
+./EvaluateJUnitXml
+
+echo "Evaluating invalid file"
+./EvaluateJUnitXml herp.derp
+
 echo "Evaluating for 0% pass"
 ./EvaluateJUnitXml test_results_0pc_pass.xml
+
 echo "Evaluating for 66% pass"
 ./EvaluateJUnitXml test_results_66pc_pass.xml
+
 echo "Evaluating for 100% pass"
 ./EvaluateJUnitXml test_results_100pc_pass.xml
 
